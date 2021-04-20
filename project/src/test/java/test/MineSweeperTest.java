c2c1d277a67984f99b96c1aa8372f0105d76ab15
Vemund Thallaug Lund <vemund@thallaug.org>
2021-04-20 19:49:47 +0200

    Noen små endringer

--- project/src/test/java/test/MineSweeperTest.java
+++ project/src/test/java/test/MineSweeperTest.java
@@ -1,15 +1,19 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import application.Board;
 import application.Game;
+import application.Square;
 
 public class MineSweeperTest {
 
     private Game game;
+    private Board board;
 
     @BeforeEach
     public void setup() {
@@ -21,6 +25,24 @@ public class MineSweeperTest {
 	for (int i = 0; i < 100; i++) {
 	    assertTrue(game.isSquareEditable(i));
 	}
+    }
+
+    @Test
+    public void checkIsEditable2() {
+	game.getBoard().getSquares().get(5).setIsEditable();
+	assertFalse(game.getBoard().getSquares().get(5).getIsEditable());
+    }
+
+    @Test
+    public void testIsBomb() {
+	// Lager et square objekt med 100% sjanse for at det er en bombe
+	Square sq = new Square(100);
+	assertTrue(sq.getIsBomb());
+    }
+
+    @Test
+    public void testIsNearBySquareEmpty() {
+
     }
 
 }
