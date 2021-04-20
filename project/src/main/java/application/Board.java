9de3c39cb982828f32c9f6627ac1379283e24985
Kristian Krøgenes <kristiankrogenes@gmail.com>
2021-04-20 11:13:48 +0200

    Cleaned up controller and game class.

--- project/src/main/java/application/Board.java
+++ project/src/main/java/application/Board.java
@@ -5,63 +5,42 @@ import java.util.Arrays;
 
 public class Board {
 
-    private ArrayList<Square> squares = new ArrayList<Square>();
-    private int totalBombs;
+	private ArrayList<Square> squares = new ArrayList<Square>();
+	private int totalBombs;
 
-    public Board() {
-	for (int i = 0; i < 100; i++) {
-	    squares.add(new Square(15));
-	}
-	setTotalNumberOfBombs();
-
-    }
-
-    public Board(String data) {
-	ArrayList<String> squaresData = new ArrayList<String>(Arrays.asList(data.split(",")));
-	for (int i = 0; i < 100; i++) {
-	    ArrayList<String> squareData = new ArrayList<String>(Arrays.asList(squaresData.get(i).split("-")));
-	    boolean isBomb = Boolean.parseBoolean(squareData.get(0));
-	    boolean isEditable = Boolean.parseBoolean(squareData.get(1));
-	    boolean isFlagged = Boolean.parseBoolean(squareData.get(2));
-	    squares.add(new Square(isBomb, isEditable, isFlagged));
-	}
-	setTotalNumberOfBombs();
-    }
-
-    public ArrayList<Square> getSquares() {
-	return this.squares;
-    }
-
-    public int getTotalBombs() {
-	return this.totalBombs;
-    }
-
-    private void setTotalNumberOfBombs() {
-	squares.stream().forEach(sq -> {
-	    if (sq.getIsBomb()) {
-		this.totalBombs++;
-	    }
-	});
-    }
-
-    public int numberOfBombsNearby(int posX, int posY) {
-
-	int bombCount = 0;
-
-	for (int x = -1; x < 2; x++) {
-	    for (int y = -1; y < 2; y++) {
-
-		int nX = posX + x;
-		int nY = posY + y;
-
-		if ((nX < 10 && nX >= 0) && (nY < 10 && nY >= 0)) {
-		    if (this.squares.get((10 * nY) + nX).getIsBomb()) {
-			bombCount++;
-		    }
+	public Board() {
+		for (int i = 0; i < 100; i++) {
+			squares.add(new Square(15));
 		}
-	    }
+		setTotalNumberOfBombs();
+
 	}
 
-	return bombCount;
-    }
+	public Board(String data) {
+		ArrayList<String> squaresData = new ArrayList<String>(Arrays.asList(data.split(",")));
+		for (int i = 0; i < 100; i++) {
+			ArrayList<String> squareData = new ArrayList<String>(Arrays.asList(squaresData.get(i).split("-")));
+			boolean isBomb = Boolean.parseBoolean(squareData.get(0));
+			boolean isEditable = Boolean.parseBoolean(squareData.get(1));
+			boolean isFlagged = Boolean.parseBoolean(squareData.get(2));
+			squares.add(new Square(isBomb, isEditable, isFlagged));
+		}
+		setTotalNumberOfBombs();
+	}
+
+	public ArrayList<Square> getSquares() {
+		return this.squares;
+	}
+
+	public int getTotalBombs() {
+		return this.totalBombs;
+	}
+
+	private void setTotalNumberOfBombs() {
+		squares.stream().forEach(sq -> {
+			if (sq.getIsBomb()) {
+				this.totalBombs++;
+			}
+		});
+	}
 }
--- project/src/main/java/application/Game.java
+++ project/src/main/java/application/Game.java
@@ -2,53 +2,87 @@ package application;
 
 public class Game {
 
-    private Board board;
-    private int numberOfRemainingBombs;
-    private boolean isGameWon;
+	private Board board;
+	private int numberOfRemainingBombs;
+	private boolean isGameWon;
 
-    public Game() {
-	this.board = new Board();
-    }
-
-    public Game(String data) {
-	this.board = new Board(data);
-    }
-
-    public Board getBoard() {
-	return board;
-    }
-
-    public boolean isGameWon() {
-	this.isGameWon = true;
-	board.getSquares().stream().forEach(sq -> {
-	    if (!((sq.getIsBomb() && sq.getIsFlagged()) || (!sq.getIsBomb() && !sq.getIsEditable()))) {
-		isGameWon = false;
-	    }
-	});
-
-	return isGameWon;
-    }
-
-    public int checkRemainingBombs() {
-	numberOfRemainingBombs = board.getTotalBombs();
-	board.getSquares().stream().forEach(sq -> {
-	    if (sq.getIsFlagged()) {
-		numberOfRemainingBombs--;
-	    }
-	});
-	return numberOfRemainingBombs;
-    }
-
-    public void openNearbySquares(int id, int posX, int posY) {
-	for (int x = -1; x < 2; x++) {
-	    for (int y = -1; y < 2; y++) {
-		int nX = posX + x;
-		int nY = posY + y;
-		if ((nX < 10 && nX >= 0) && (nY < 10 && nY >= 0) && !(posX == nX && posY == nY)
-			&& !(this.getBoard().getSquares().get((10 * nY) + nX).getIsBomb())) {
-		    checkSquare((10 * nY) + nX, nX, nY);
-		}
-	    }
+	public Game() {
+		this.board = new Board();
 	}
-    }
+
+	public Game(String data) {
+		this.board = new Board(data);
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public boolean isGameWon() {
+		this.isGameWon = true;
+		board.getSquares().stream().forEach(sq -> {
+			if (!((sq.getIsBomb() && sq.getIsFlagged()) || (!sq.getIsBomb() && !sq.getIsEditable()))) {
+				isGameWon = false;
+			}
+		});
+
+		return isGameWon;
+	}
+	
+	public int numberOfBombsNearby(int posX, int posY) {
+
+		int bombCount = 0;
+
+		for (int x = -1; x < 2; x++) {
+			for (int y = -1; y < 2; y++) {
+
+				int nX = posX + x;
+				int nY = posY + y;
+
+				if ((nX < 10 && nX >= 0) && (nY < 10 && nY >= 0)) {
+					if (board.getSquares().get((10 * nY) + nX).getIsBomb()) {
+						bombCount++;
+					}
+				}
+			}
+		}
+
+		return bombCount;
+	}
+
+	public int checkRemainingBombs() {
+		numberOfRemainingBombs = board.getTotalBombs();
+		board.getSquares().stream().forEach(sq -> {
+			if (sq.getIsFlagged()) {
+				numberOfRemainingBombs--;
+			}
+		});
+		return numberOfRemainingBombs;
+	}
+
+	public void openNearbySquares(int id, int posX, int posY) {
+		for (int x = -1; x < 2; x++) {
+			for (int y = -1; y < 2; y++) {
+				int nX = posX + x;
+				int nY = posY + y;
+				if ((nX < 10 && nX >= 0) && (nY < 10 && nY >= 0) && !(posX == nX && posY == nY)
+						&& !(this.getBoard().getSquares().get((10 * nY) + nX).getIsBomb())) {
+					//checkSquare((10 * nY) + nX, nX, nY);
+					checkSquare((10 * nY) + nX);
+				}
+			}
+		}
+	}
+	
+	private int checkSquare(int id) {
+		return id;
+	}
+	
+	public boolean isSquareEditable(int id) {
+		return board.getSquares().get(id).getIsEditable();
+	}
+	 
+	public boolean isSquareBomb(int id) {
+		return board.getSquares().get(id).getIsBomb();
+	}
 }
